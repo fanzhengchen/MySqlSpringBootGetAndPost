ac0ad91331fc321ef1b74bd94566b4ffbf75be9d
fanzhengchen <fanzhengchen@gmail.com>
2016-09-28 20:30:05 +0800

    init

---
+++ src/main/java/com/example/controller/ProductsController.java
@@ -0,0 +1,20 @@
+package com.example.controller;
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by mark on 16-9-28.
+ */
+@RestController("/products")
+public class ProductsController {
+
+
+    @RequestMapping(value = "/{page}", method = RequestMethod.GET)
+    public String getProducts(@PathVariable("page") String page) {
+
+        return "getProducts";
+    }
+}
---
+++ src/main/java/com/example/entity/User.java
@@ -0,0 +1,43 @@
+package com.example.entity;
+
+import javax.annotation.Generated;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+/**
+ * Created by mark on 16-9-28.
+ */
+@Entity
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+    private String name;
+    private String gender;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+}
